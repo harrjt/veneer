d3efd3875f1e2414b18246daeda8c5e68c7922f0
harrjt <66974709+harrjt@users.noreply.github.com>
2020-06-28 21:49:50 -0400

    Moved wallet update to buy_artwork
    Replaced `veneer = Marketplace()` with `market_list = Marketplace()`

--- veneer.py
+++ veneer.py
@@ -1,3 +1,4 @@
+""" market_list.py -- Codecademy project in Computer Science Course """
 # pylint: disable=C0114
 # pylint: disable=C0115
 # pylint: disable=C0116
@@ -34,11 +35,18 @@ class Marketplace:
 
 
 class Client:
+
+    num_clients = 0
+
     def __init__(self, name, location, is_museum, wallet):
         self.name = name
         self.is_museum = is_museum
         self.location = location if is_museum else "Private Collection"
         self.wallet = wallet
+        self.wishlist = []
+        self.bucketlist = []
+
+        Client.num_clients += 1
 
     def __str__(self):
         return "{}, {}, ${:,}.".format(
@@ -47,28 +55,27 @@ class Client:
     def sell_artwork(self, artwork, price):
         if artwork.owner == self:
             new_listing = Listing(artwork, price, self)
-            veneer.add_listing(new_listing)
-            self.wallet += new_listing.price
+            market_list.add_listing(new_listing)
 
     def buy_artwork(self, artwork, price):
         if artwork.owner != self:
             art_listing = None
-            for listing in veneer.listings:
+            for listing in market_list.listings:
                 if listing.art == artwork:
                     art_listing = listing
                     break
             if art_listing is not None:
+                art_listing.art.owner.wallet += price
                 art_listing.art.owner = self
-                veneer.remove_listing(art_listing)
+                market_list.remove_listing(art_listing)
                 self.wallet -= price
 
     def create_wishlist(self):
-        self.wishlist = []
+
         for item in self.bucketlist:
-            for listing in veneer.listings:
+            for listing in market_list.listings:
                 if item.title == listing.art.title:
                     self.wishlist.append(listing.art)
-
 
     def show_wishlist(self):
         for item in self.wishlist:
@@ -97,11 +104,11 @@ def print_status(collectors, pieces):
         print(piece)
 
 
-edytta = Client("Edytta Halpirt", None, False, 15000000)
-moma = Client("The MOMA", "New York", True, 100000000)
-james = Client("James Bond", None, False, 18000000)
-thurston = Client("Thurston Howell III", None, False, 10000000)
-npm = Client("The National Pushkin Museum", "Moscow", True, 30000000)
+edytta = Client("Edytta Halpirt", None, False, 15_000_000)
+moma = Client("The MOMA", "New York", True, 100_000_000)
+james = Client("James Bond", None, False, 18_000_000)
+thurston = Client("Thurston Howell III", None, False, 10_000_000)
+npm = Client("The National Pushkin Museum", "Moscow", True, 30_000_000)
 clients = (moma, npm, edytta, thurston, james)
 
 girl_with_a_mandolin = Art("Picasso, Pablo", "Girl with a Mandolin (Fanny Tellier)", 1910, "oil on canvas", edytta)
@@ -113,26 +120,26 @@ art_pieces = (girl_with_a_mandolin, vétheuil_in_the_fog,
 
 print_status(clients, art_pieces)
 
-veneer = Marketplace()
+market_list = Marketplace()
 print("Listings: (empty -- nothing for sale)")
-veneer.show_listings()
+market_list.show_listings()
 print()
 
-edytta.sell_artwork(girl_with_a_mandolin, 6000000)
+edytta.sell_artwork(girl_with_a_mandolin, 6_000_000)
 print("Listings: (\"Girl with a Mandolin\" for sale)")
-veneer.show_listings()
+market_list.show_listings()
 print()
 
-moma.buy_artwork(girl_with_a_mandolin, 6000000)
+moma.buy_artwork(girl_with_a_mandolin, 6_000_000)
 print("Listings: (empty -- \"Girl with a Mandolin\" sold; nothing for sale)")
-veneer.show_listings()
+market_list.show_listings()
 print()
 
 print_status(clients, art_pieces)
 
-moma.sell_artwork(the_starry_night, 8000000)
+moma.sell_artwork(the_starry_night, 8_000_000)
 print("Listings:")
-veneer.show_listings()
+market_list.show_listings()
 
 edytta.bucketlist = (the_red_vineyard, the_starry_night)
 print("Edytta's bucket list:")
